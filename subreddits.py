705ffc1a9940d5906c0d4e09d88ec102681c37da
Metaljerk <scottlonghofer@gmail.com>
2021-01-30 15:03:51 -0500

    update

--- subreddits.py
+++ subreddits.py
@@ -1,1 +1,1 @@
-
+learnprogramming
--- translatorbot.py
+++ translatorbot.py
@@ -1,7 +1,10 @@
-import praw
 import translators as ts
+from config import reddit
 
 # Inspiration post
-user_text = 'ch hätte gerne, dass die Schleife jetzt mit dem Switch Befehl fortgeführt wird, da komme ich leider nicht mehr weiter. Das Ganze ist eine Schulaufgabe und ich komme nicht so richtig weiter Mit freundlichen Grüßen!'
+# user_text = 'ch hätte gerne, dass die Schleife jetzt mit dem Switch Befehl fortgeführt wird, da komme ich leider nicht mehr weiter. Das Ganze ist eine Schulaufgabe und ich komme nicht so richtig weiter Mit freundlichen Grüßen!'
 
-print(ts.google(user_text))
+    
+for submission in reddit.subreddit('learnprogramming').stream.submissions():
+    user_text=submission.selftext
+    print(ts.google(user_text))
