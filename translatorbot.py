6bfaacb9a416dae2e91c881e4f77ce8d614165ea
Metaljerk <scottlonghofer@gmail.com>
2021-01-30 15:40:56 -0500

    proof of concept

--- translatorbot.py
+++ translatorbot.py
@@ -1,10 +1,21 @@
-import translators as ts
+import translators as translate
 from config import reddit
+from langdetect import detect
 
 # Inspiration post
 # user_text = 'ch hätte gerne, dass die Schleife jetzt mit dem Switch Befehl fortgeführt wird, da komme ich leider nicht mehr weiter. Das Ganze ist eine Schulaufgabe und ich komme nicht so richtig weiter Mit freundlichen Grüßen!'
 
     
-for submission in reddit.subreddit('learnprogramming').stream.submissions():
+for submission in reddit.subreddit('learnspanish').stream.submissions():
     user_text=submission.selftext
-    print(ts.google(user_text))
+    try:
+        language = detect(user_text)
+    except Exception:
+        pass
+    else:
+        if language != 'en':
+            try:
+                print(user_text)
+                print(translate.google(user_text))
+            except Exception:
+                pass
